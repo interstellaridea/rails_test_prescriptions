b58e710f2a2354e15fe4de57facb4e9e5f33d916
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-06-22 01:23:39 -0700

    validates project name, renders new on failed created project

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,11 +2,18 @@ require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
 		describe 'POST#create' do
-			it 'creates a project' do
 
+			it 'creates a project' do
 				post :create, params: {project: { name: 'Runway', tasks: 'Start something:2' }}
 				expect(response).to redirect_to(projects_path)
 				expect(assigns(:action).project.name).to eq('Runway')
+			end
+
+			it 'goes back to form on failure' do
+				# create project without name, checking validations
+				post :create, params: {project: {name: '', tasks: ''}}
+				expect(response).to render_template(:new)
+				expect(assigns(:project)).to be_present
 			end
 		end
 
